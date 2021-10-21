56c9538c2e400b92d92967a6c059b73e423523ca
Vlado <andricvlado@gmail.com>
2021-10-21 23:14:15 +0200

    passing arguments to python script

--- Access_Template_argv.py
+++ Access_Template_argv.py
@@ -1,4 +1,10 @@
+# Passing arguments to Access_Template script
 
+from sys import argv
+
+
+interface = argv[1]
+vlan = argv[2]
 
 access_template = ['switchport mode access',
                    'switchport access vlan {}',
@@ -6,4 +12,5 @@ access_template = ['switchport mode access',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable']
 
-print('\n'.join(access_template).format(5))
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
